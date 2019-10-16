55a3f18ad399b316b60acde796ece578831b28a3
okano_note <okanosyogo@gmail.com>
2019-10-16 22:01:46 +0900

    [update] font test

--- src/components/card_cluster.vue
+++ src/components/card_cluster.vue
@@ -34,91 +34,91 @@ export default {
   },
   props: ['recog_state'],
   methods: {
-    ai_speaker_request(body_text) {
-        axios.post(this.ai_speaker_endpoint, {text: body_text})
-        .then(res => {
-            if(res.status === 200){
-                this.addResult(res.data.text, res.data.question)
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    },
-    text2speech_request(body_text) {
-        function string_to_buffer(src) {
-          return (new Uint16Array([].map.call(src, function(c) {
-            return c.charCodeAt(0)
-          }))).buffer;
-        }
+      ai_speaker_request(body_text) {
+          axios.post(this.ai_speaker_endpoint, {text: body_text})
+          .then(res => {
+              if(res.status === 200){
+                  this.addResult(res.data.text, res.data.question)
+              }
+          })
+          .catch(error => {
+              console.log(error);
+          });
+      },
+      text2speech_request(body_text) {
+          function string_to_buffer(src) {
+            return (new Uint16Array([].map.call(src, function(c) {
+              return c.charCodeAt(0)
+            }))).buffer;
+          }
 
-        var dogBarkingBuffer = null;
-        // Fix up prefixing
-        window.AudioContext = window.AudioContext || window.webkitAudioContext;
-        var context = new AudioContext();
+          var dogBarkingBuffer = null;
+          // Fix up prefixing
+          window.AudioContext = window.AudioContext || window.webkitAudioContext;
+          var context = new AudioContext();
 
-        axios.post(this.speech2text_endpoint, {
-            Command:"AP_Synth",
-            SpeakerID:"1",
-            StyleID:"1",
-            SpeechRate:"1.15",
-            AudioFileFormat:"2",
-            TextData: body_text
-        }, 'arraybuffer')
-        .then(res => { // res.data
-            //let arraybuffer = string_to_buffer(res.data)
-            let arraybuffer = res.data
-            console.log(typeof arraybuffer);
-            context.decodeAudioData(arraybuffer, function(buffer) {
-              dogBarkingBuffer = buffer;
-            },
-            function(error) {
-                console.error('decodeAudioData error', error);
-            });
-        })
-        .catch(error => {
-                console.log(error);
-        });
-    },
-    addResult(text, question){
-        this.responses.unshift({text: text, question: question})
-        //console.log(this.responses);
-    }
+          axios.post(this.speech2text_endpoint, {
+              Command:"AP_Synth",
+              SpeakerID:"1",
+              StyleID:"1",
+              SpeechRate:"1.15",
+              AudioFileFormat:"2",
+              TextData: body_text
+          }, 'arraybuffer')
+          .then(res => { // res.data
+              //let arraybuffer = string_to_buffer(res.data)
+              let arraybuffer = res.data
+              console.log(typeof arraybuffer);
+              context.decodeAudioData(arraybuffer, function(buffer) {
+                dogBarkingBuffer = buffer;
+              },
+              function(error) {
+                  console.error('decodeAudioData error', error);
+              });
+          })
+          .catch(error => {
+                  console.log(error);
+          });
+      },
+      addResult(text, question){
+          this.responses.unshift({text: text, question: question})
+          //console.log(this.responses);
+      }
 
   },
 
   created: function() {
-    this.recognition.lang = "ja-JP";
-    this.recognition.continuous = false;
-    let that = this
-    this.recognition.onresult = function(event) {
-        if (event.results.length > 0) {
-            let result = event.results[0][0].transcript;
-            // ウェイクアップワードを含むかどうかを判別せずに日常会話かもしれないテキストを全てサーバに送るのはプライバシー・セキュリティ上の不安があるが、サーバサイドにロジックを集中することを優先
-            that.ai_speaker_request(result)
-        }
-        that.recognition.stop();
-    }
-    this.recognition.onend = () => {
-      // Continue
-      let switch_check = setInterval(function() {
-          if (that.recog_state === true){
-            clearInterval(switch_check);
-            that.recognition.start();
+      this.recognition.lang = "ja-JP";
+      this.recognition.continuous = false;
+      let that = this
+      this.recognition.onresult = function(event) {
+          if (event.results.length > 0) {
+              let result = event.results[0][0].transcript;
+              // ウェイクアップワードを含むかどうかを判別せずに日常会話かもしれないテキストを全てサーバに送るのはプライバシー・セキュリティ上の不安があるが、サーバサイドにロジックを集中することを優先
+              that.ai_speaker_request(result)
           }
-      }, 500);
-    };
+          that.recognition.stop();
+      }
+      this.recognition.onend = () => {
+        // Continue
+        let switch_check = setInterval(function() {
+            if (that.recog_state === true){
+              clearInterval(switch_check);
+              that.recognition.start();
+            }
+        }, 500);
+      };
 
-    this.recognition.start();
+      this.recognition.start();
 
-    // For tutorial
-    this.addResult("マイクを許可して、 「ハローワールド、チュートリアルについて検索」 と言って見ましょう", "チュートリアル")
+      // For tutorial
+      this.addResult("マイクを許可して、 「ハローワールド、チュートリアルについて検索」 と言って見ましょう", "チュートリアル")
 
-    // For TTS test
-    //this.text2speech_request("音声合成エンジンによる音声です")
+      // For TTS test
+      //this.text2speech_request("音声合成エンジンによる音声です")
 
-    // For network test
-    this.ai_speaker_request("ハローワールド タピオカ")
+      // For network test
+      this.ai_speaker_request("ハローワールド タピオカ")
   },
 }
 
--- src/components/explanation.vue
+++ src/components/explanation.vue
@@ -1,6 +1,12 @@
 <template>
     <v-layout justify-center wrap>
         <h2 class="headline font-weight-bold mb-0" >"ハローワールド"と呼びかけてから、 調べたい単語を質問してみてください</h2>
+        <h2 class="headline" >"ハローワールド"と呼びかけてから、 調べたい単語を質問してみてください</h2>
+        <h2 class=".subtitle-1 mb-3">Regular 16sp</h2>
+        <h2 class=".subtitle-1 .font-weight-light">Regular 16sp</h2>
+        <h2 class=".subtitle-1 .font-weight-regular">Regular 16sp</h2>
+        <h2 class=".subtitle-2 .font-weight-medium">Mid 16sp</h2>
+        <h2 class=".body-2 mb-3">Regular 14sp</h2>
     </v-layout>
 </template>
 
