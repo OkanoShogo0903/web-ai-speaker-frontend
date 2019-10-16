20d04f8b5f5dde7e88a8179295704e75a24c4de4
okano_note <okanosyogo@gmail.com>
2019-10-16 22:36:56 +0900

    [update] font test6

--- src/components/explanation.vue
+++ src/components/explanation.vue
@@ -1,14 +1,6 @@
 <template>
     <v-layout justify-center wrap>
         <h4 class=".overline font-weight-bold mb-0" >"ハローワールド"と呼びかけてから、 調べたい単語を質問してみてください</h4>
-  	
-        <div class=".overline mb-12">div overline 1</div>
-        <h1 class=".overline mb-12">overline 1</h1>
-        <h2 class=".overline mb-12">overline 2</h2>
-        <h3 class=".overline mb-12">overline 3</h3>
-        <h4 class=".overline mb-12">overline 4</h4>
-        <h5 class=".overline mb-12">overline 5</h5>
-        <h5 class=".overline mb-12">overline 6</h5>
     </v-layout>
 </template>
 
