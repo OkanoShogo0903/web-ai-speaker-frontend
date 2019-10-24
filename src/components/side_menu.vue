428912f2f8096c97f1b5379e64e3be3feb11f5fd
okano_note <okanosyogo@gmail.com>
2019-10-24 19:37:44 +0900

    [update] アイコンのロードの仕方変えてみた

--- src/components/side_menu.vue
+++ src/components/side_menu.vue
@@ -4,6 +4,9 @@
     fixed
     app
   >
+
+    <v-icon>mdi-settings-outline</v-icon>
+    <v-icon>mdi-anchor</v-icon>
     <v-list>
       <v-list-item
         v-for="item in items"
@@ -19,10 +22,38 @@
         </v-list-item-content>
       </v-list-item>
     </v-list>
+
+
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-icon>{{ icons.mdiAccount }}</v-icon>
+      <div class="mx-2"></div>
+      <v-icon>{{ icons.mdiPencil }}</v-icon>
+      <div class="mx-2"></div>
+      <v-icon>{{ icons.mdiShareVariant }}</v-icon>
+      <div class="mx-2"></div>
+      <v-btn
+        color="primary"
+        depressed
+      >
+        <v-icon left>{{ icons.mdiDelete }}</v-icon>
+        Delete
+      </v-btn>
+    </v-row>
+
   </v-navigation-drawer>
 </template>
 
 <script>
+  import {
+    mdiAccount,
+    mdiPencil,
+    mdiShareVariant,
+    mdiDelete,
+  } from '@mdi/js'
+import { mdiSettingsOutline } from '@mdi/js';
 export default {
     data() {
         return {
@@ -31,6 +62,12 @@ export default {
             { title: 'Account', icon: 'account_box' },
             { title: 'Admin', icon: 'gavel' },
           ],
+          icons: {
+            mdiAccount,
+            mdiPencil,
+            mdiShareVariant,
+            mdiDelete,
+          },
         }
     }
 }
